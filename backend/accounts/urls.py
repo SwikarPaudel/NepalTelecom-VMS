a48c9f204deb79f212c0435a927680e9fba2199e
Swikar Paudel <141716870+SwikarPaudel@users.noreply.github.com>
2026-06-04 14:20:58 +0545

    urls of account is added

---
+++ backend/accounts/urls.py
@@ -0,0 +1,10 @@
+from django import admin
+from django.urls import path
+from .views import RegisterUserView, UserProfileViewSet,UserProfileDetailView,AdminProfileManagementViewSet
+
+urlpatterns = [
+    path('newregistration/', RegisterUserView.as_view(), name='new_registration'),
+    path('userprofileslist/', UserProfileViewSet.as_view({'get': 'list'}), name='user-profiles'),
+    path('userdetails/', UserProfileDetailView.as_view({'get': 'retrieve','patch': 'partial_update','put': 'update' }),name='user-profile-detail'),
+    path('admin/manageprofiles/<int:pk>/', AdminProfileManagementViewSet.as_view({'get': 'list','put': 'update','patch': 'partial_update' }), name='admin-manage-profiles'),
+]

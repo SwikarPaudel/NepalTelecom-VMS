ae479dec63be9a26b3d2598645c1bc3b22b49b21
Swikar Paudel <141716870+SwikarPaudel@users.noreply.github.com>
2026-06-08 12:13:17 +0545

    saved

--- backend/bookings/models.py
+++ backend/bookings/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from branch.models import Branch
+from driver.models import DriverProfile
 
 # Create your models here.
+class Booking(models.Model):
+    _STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('APPROVED', 'Approved'),
+        ('REJECTED', 'Rejected'),
+        ('ONGOING', 'Ongoing'),
+        ('COMPLETED', 'Completed'),
+        ('CANCELLED', 'Cancelled'),
+    ]
+
+    
+
+    def __str__(self):
+        return f"Booking {self.id} - Driver: {self.driver.user.username} - Branch: {self.branch.name}"
